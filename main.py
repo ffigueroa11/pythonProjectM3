03e2c025ad0dd753baca966d6fd0a4dc897a448c
Francisco Figueroa <francisco.figueroa@hotmail.cl>
2024-09-02 20:27:15 -0400

    se separan listas

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # Lista inicial de nombres
 listNombres = ['Harry Houdini', 'Newton', 'David Blaine', 'Hawking', 'Messi', 'Teller', 'Einstein', 'Pele', 'Juanes']
 
+
+grupoMagos       = ['Harry Houdini', 'David Blaine', 'Teller']
+grupoCientificos = ['Newton', 'Hawking', 'Einstein']
+
+listMagos       = [nombre for nombre in listNombres if nombre in grupoMagos]
+listCientificos = [nombre for nombre in listNombres if nombre in grupoCientificos]
+listOtros       = [nombre for nombre in listNombres if nombre not in grupoMagos and nombre not in grupoCientificos]
+
+print(listMagos)
+print(listCientificos)
+print(listOtros)
+
+
+
